c96b50dbea7a7c4ed7e19cd249a7c0035c84e559
Elias <elias.staehle@gmail.com>
2022-09-13 21:35:27 -0400

    re-added console logs

--- js/app.js
+++ js/app.js
@@ -16,12 +16,14 @@ button.forEach(button =>
       quizStart = prompt('that was not an answer choice, again answer with only "y", "yes", "n", or "no"').toLowerCase();
     }
     if (quizStart === 'y' || quizStart === 'yes') {
+      // console.log('okay lets begin');
       alert('okay lets begin');
     }
     else {
       while (quizStart === 'n' || quizStart === 'no') {
         quizStart = prompt('you need to understand please answer with "y", or "yes"').toLowerCase();
       } if (quizStart === 'y' || quizStart === 'yes') {
+        // console.log('okay lets begin');
         alert('okay lets begin');
       }
     }
@@ -33,11 +35,13 @@ button.forEach(button =>
       questionOne = prompt('that was not an answer choice, again answer with only "y", "yes", "n", or "no"').toLowerCase();
     }if (questionOne === 'n' || questionOne === 'no')
     {
+      // console.log('yes! thats correct next question!');
       alert('yes! thats correct next question!');
     }else{
       while (questionOne === 'y' || questionOne === 'yes') {
         questionOne = prompt('That is incorrect please read my bio again and try again. Am i 26 years old?').toLowerCase();
       } if (questionOne === 'n' || questionOne === 'no') {
+        // console.log('correct next question!');
         alert('correct next question!');
       }
     }
@@ -48,11 +52,13 @@ button.forEach(button =>
       questionTwo = prompt('that was not an answer choice, again answer with only "y", "yes", "n", or "no"').toLowerCase();
     }if (questionTwo === 'y' || questionTwo === 'yes')
     {
+      // console.log('yes! thats correct next question!');
       alert('yes! thats correct next question!');
     }else{
       while (questionTwo === 'n' || questionTwo === 'no') {
         questionTwo = prompt('That is incorrect please read my bio again and try again. Was I in the Marine Corps?').toLowerCase();
       } if (questionTwo === 'y' || questionTwo === 'yes') {
+        // console.log('correct next question!');
         alert('correct next question!');
       }
     }
@@ -62,11 +68,13 @@ button.forEach(button =>
       questionThree = prompt('that was not an answer choice, again answer with only "y", "yes", "n", or "no"').toLowerCase();
     }if (questionThree === 'n' || questionThree === 'no')
     {
+      // console.log('yes! thats correct next question!');
       alert('yes! thats correct next question!');
     }else{
       while (questionThree === 'y' || questionThree === 'yes') {
         questionThree = prompt('That is incorrect please read my bio again and try again. Did I teach people how to swim?').toLowerCase();
       } if (questionThree === 'n' || questionThree === 'no') {
+        // console.log('correct next question!');
         alert('correct next question!');
       }
     }
@@ -76,11 +84,13 @@ button.forEach(button =>
       questionFour = prompt('that was not an answer choice, again answer with only "y", "yes", "n", or "no"').toLowerCase();
     }if (questionFour === 'n' || questionFour === 'no')
     {
+    //  console.log('yes! thats correct next question!');
       alert('yes! thats correct next question!');
     }else{
       while (questionFour === 'y' || questionFour === 'yes') {
         questionFour = prompt('That is incorrect please read my bio again and try again. Am I trying to become a Cybersecurity pro?').toLowerCase();
       } if (questionFour === 'n' || questionFour === 'no') {
+        // console.log('correct next question!');
         alert('correct next question!');
       }
     }
@@ -90,7 +100,8 @@ button.forEach(button =>
       questionFive = prompt('that was not an answer choice, again answer with only "y", "yes", "n", or "no"').toLowerCase();
     }if (questionFive === 'y' || questionFive === 'yes')
     {
-      alert('you got them all right!')
+      // console.log('you got them all right!');
+      alert('you got them all right!');
       const audio = new Audio();
       audio.src = "victoryyyyyyy.mp3";
       audio.play();
@@ -98,13 +109,14 @@ button.forEach(button =>
       while (questionFive === 'n' || questionFive === 'no') {
         questionFive = prompt('That is incorrect please read my bio again and try again. Is playing video games my favorite thing to do?').toLowerCase();
       } if (questionFive === 'y' || questionFive === 'yes') {
+        // console.log('you got them all right!');
         alert('you got them all right!');
         const audio = new Audio();
       audio.src = "victoryyyyyyy.mp3";
       audio.play();
       }
     }
-
+    // console.log(`${user} i hope you had a good time on my site and learned a bit about me :)`)
     alert(`${user} i hope you had a good time on my site and learned a bit about me :)`)
   }
   )

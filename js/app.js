b71149f600f0ca33d7317f96357a083f8ce9e155
Elias <elias.staehle@gmail.com>
2022-09-15 19:22:29 -0400

    fixed function for question 6

--- js/app.js
+++ js/app.js
@@ -12,28 +12,20 @@ const button = document.querySelectorAll('button');
 
 button.forEach(button =>
   button.addEventListener('click', () => {
-    let quizStart = prompt('Please answer the following questions with "y", "yes", "n", or "no". Do you understand?').toLowerCase();
+    
+    function startingQuiz(){
+      let quizStart = prompt('Please answer the following questions with "y", "yes", "n", or "no". Do you understand?').toLowerCase();
+      quizStart(quizStart);
+    }
 
-    while (quizStart !== 'y' && quizStart !== 'yes' && quizStart !== 'n' && quizStart !== 'no') {
-      quizStart = prompt('that was not an answer choice, again answer with only "y", "yes", "n", or "no"').toLowerCase();
-    }
-    if (quizStart === 'y' || quizStart === 'yes') {
-      // console.log('okay lets begin');
-      alert('okay lets begin');
-    }
-    else {
-      while (quizStart === 'n' || quizStart === 'no') {
-        quizStart = prompt('you need to understand please answer with "y", or "yes"').toLowerCase();
-      } if (quizStart === 'y' || quizStart === 'yes') {
-        // console.log('okay lets begin');
-        alert('okay lets begin');
-      }
-    }
+  startOfQuiz();
+
     function questionNumOne() {
       // let questionNum = 1;
       let asking = prompt('Am I 26 years old?');
       ansNo(asking);
     }
+
     questionNumOne();
 
     function questionNumTwo() {
@@ -58,29 +50,7 @@ button.forEach(button =>
       ansYes(asking);
     }
     questionNumFive();
-    function numGuessGame() {
-      let numGuess = 0;
-      let guessesLeft = 0;
-      const myNum = 15;
-      while (guessesLeft < 4 && numGuess !== myNum) {
-        numGuess = parseInt(prompt('Guess the number 1-20'))
-        if (numGuess < myNum) {
-          alert('No go higher')
-          guessesLeft++;
-          alert(`you got ${4 - guessesLeft}, guess remaining.`);
-        } else if (numGuess > myNum) {
-          alert('No go lower');
-          guessesLeft++;
-          alert(`you got ${4 - guessesLeft}, guess remaining.`);
-        } else if (numGuess == myNum) {
-          alert("yeah thats it good job yay you wow youre so cool omgosh how did you know you must be so smart i can beleive you got it in only four guess how the heck i am bamboozled to say the least flabbergasted if you will this is just a revolutionary expierience");
-          score++;
-        }
-        if (guessesLeft === 4) {
-          alert('you have run out of chances.');
-        }
-      }
-    }
+
     numGuessGame();
 
     guessGame();
--- js/func.js
+++ js/func.js
@@ -1,4 +1,21 @@
 'use strict'
+function startOfQuiz(quizStart){
+  while (quizStart !== 'y' && quizStart !== 'yes' && quizStart !== 'n' && quizStart !== 'no') {
+    quizStart = prompt('that was not an answer choice, again answer with only "y", "yes", "n", or "no"').toLowerCase();
+  }
+  if (quizStart === 'y' || quizStart === 'yes') {
+    // console.log('okay lets begin');
+    alert('okay lets begin');
+  }
+  else {
+    while (quizStart === 'n' || quizStart === 'no') {
+      quizStart = prompt('you need to understand please answer with "y", or "yes"').toLowerCase();
+    } if (quizStart === 'y' || quizStart === 'yes') {
+      // console.log('okay lets begin');
+      alert('okay lets begin');
+    }
+  }
+}
 function ansNo(asking) {
   while (asking !== 'y' && asking !== 'yes' && asking !== 'n' && asking !== 'no') {
     asking = prompt('that was not an answer choice, again answer with only "y", "yes", "n", or "no" ').toLowerCase();
@@ -29,6 +46,32 @@ function ansYes(asking) {
       // console.log('correct next question!');
       alert('correct next question!');
       score++;
+    }
+  }
+}
+function numGuessGame() {
+  let numGuess = 0;
+  let guessesLeft = 0;
+  const myNum = 15;
+  while (guessesLeft < 4 && numGuess !== myNum) {
+    numGuess = parseInt(prompt('Guess the number 1-20'))
+    if (numGuess < myNum) {
+      alert('No go higher')
+      guessesLeft++;
+      alert(`you got ${4 - guessesLeft}, guess remaining.`);
+    } else if (numGuess > myNum) {
+      alert('No go lower');
+      guessesLeft++;
+      alert(`you got ${4 - guessesLeft}, guess remaining.`);
+    } else if (numGuess == myNum) {
+      alert("yeah thats it good job yay you wow youre so cool omgosh how did you know you must be so smart i can beleive you got it in only four guess how the heck i am bamboozled to say the least flabbergasted if you will this is just a revolutionary expierience");
+      score++;
+      const audio = new Audio();
+          audio.src = "victoryyyyyyy.mp3";
+          audio.play();
+    }
+    if (guessesLeft === 4) {
+      alert('you have run out of chances.');
     }
   }
 }

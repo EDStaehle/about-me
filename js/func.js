922f9a4ec3736073bf4c92002d181b832e70a036
Trace Dugar <tracedugar89@gmail.com>
2022-09-15 15:55:18 -0700

    Added function for car game, moved base code to func.js

--- js/func.js
+++ js/func.js
@@ -28,8 +28,46 @@ function ansYes(asking) {
     } if (asking === 'y' || asking === 'yes') {
       // console.log('correct next question!');
       alert('correct next question!');
-     score++;
+      score++;
     }
   }
 }
-    
+function guessGame() {
+  let carBrand = ['nissan', 'mitsubishi', 'mclarren', 'ford', 'audi'];
+  let correct = false;
+  // let numAns = 0;
+  let userAttempts = 6;
+  for (let i = 0; i < userAttempts; i++) {
+    let quetion = prompt('what is my favorite car brand?').toLowerCase();
+    for (let j = 0; j < carBrand.length; j++) {
+      if (quetion === carBrand[j]) {
+        alert(`yes thats correct, i love ${carBrand[j]}`);
+        correct = true;
+        score++;
+        break;
+      } else {
+        while (quetion !== carBrand[j] && userAttempts > 0) {
+          quetion = prompt('thta is not correct what is my favoirte car brand').toLowerCase();
+          userAttempts--;
+          alert('you have ' + userAttempts + ' attempts left');
+        } if (quetion === carBrand[j]) {
+          alert('yes that is correct');
+          correct = true;
+          score++;
+          break;
+        }
+
+      }
+
+
+    }
+    if (correct === true) {
+      break;
+    }
+
+  }
+  alert(`possible answers were ${carBrand}`);
+  alert(`your score is ${score} good job`);
+
+
+}
